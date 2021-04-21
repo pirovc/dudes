48abed8954c17dca26e8309b88aa097000b18b3c
Henning Schiebenhoefer <henning.schiebenhoefer@posteo.de>
2021-04-21 17:22:01 +0200

    add uniprot support to load_refid2taxid_files db-generation function

--- DUDesDB.py
+++ DUDesDB.py
@@ -213,16 +213,37 @@ def load_refid2taxid_files(refid2taxid_files, ref_mode, refids):
                 header=None, dtype=int,
                 converters={0: lambda x: refids_lookup[x] if x in refids else np.nan}
             ).dropna(how='any'), dtype=int))
-    else:
+    elif ref_mode == "av":
         for file in refid2taxid_files:
             refid_taxid.append(np.array(
                 pd.read_csv(file, compression='gzip' if file.endswith(".gz") else None, sep='\t', header=None,
                             skiprows=1, usecols=[1, 2],
                             converters={1: lambda x: refids_lookup[x] if x in refids else np.nan}).dropna(how='any'),
                 dtype=int))
+    else:    # ref_mode == "up"
+        for file in refid2taxid_files:
+            refid_taxid.append(np.array(
+                pd.read_csv(file, compression='gzip' if file.endswith(".gz") else None, sep='\t', header=None,
+                            usecols=[0, 12],
+                            converters={0: lambda x: refids_lookup[x] if x in refids else np.nan}).dropna(how='any'),
+                dtype=int))
     # Concatenate files together
     refid_taxid = np.concatenate(refid_taxid, axis=0)
     return refid_taxid, refids_lookup
+
+
+def test_load_refid2taxid_files_up():
+    refids = {'Q6GZX4', 'Q6GZX3', 'Q197F8', 'Q197F7', 'Q6GZX2', 'Q6GZX1', 'Q197F5', 'Q6GZX0', 'Q91G88', 'Q6GZW9', 'Q6GZW8', 'Q197F3', 'Q197F2', 'Q6GZW6', 'Q91G85', 'Q6GZW5'}
+    refid2taxid_files = ["test/ressource/idmapping_selected-test.tab"]
+    refid_taxid, refids_lookup = load_refid2taxid_files(refid2taxid_files, "up", refids)
+    expected_refid_taxid = np.array([[0, 654924], [1, 654924], [2, 345201], [3, 345201], [4, 654924], [5, 654924],
+                                     [6, 345201], [7, 654924], [8, 176652], [9, 654924], [10, 654924], [11, 345201],
+                                     [12, 345201], [13, 654924], [14, 176652], [15, 654924]])
+    expected_refids_lookup = {
+        'Q6GZX4': 0, 'Q6GZX3': 1, 'Q197F8': 2, 'Q197F7': 3, 'Q6GZX2': 4, 'Q6GZX1': 5, 'Q197F5': 6, 'Q6GZX0': 7,
+        'Q91G88': 8, 'Q6GZW9': 9, 'Q6GZW8': 10, 'Q197F3': 11, 'Q197F2': 12, 'Q6GZW6': 13, 'Q91G85': 14, 'Q6GZW5': 15}
+    assert np.array_equal(expected_refid_taxid, refid_taxid)
+    assert expected_refids_lookup == refids_lookup
 
 
 def load_nodes(nodes_file):
@@ -256,7 +277,7 @@ def get_reference_identifiers(fasta_files, ref_mode):
         cmd = 'zgrep -h -o "^>[A-Z0-9_\.]*" ' + " ".join(fasta_files) + ' | sed "s/>//g"'
     elif ref_mode == "up":
         cmd = 'zgrep -hoE "^>..\|[^|\s]+" ' + " ".join(fasta_files) + ' | sed "s/>..|//g"'
-    return_code, out = subprocess.getstatusoutput("set -o pipefail; "+cmd)
+    return_code, out = subprocess.getstatusoutput("set -o pipefail; " + cmd)
     if return_code == 0:
         refids = set(l for l in out.split('\n') if l)
     else:    # python
