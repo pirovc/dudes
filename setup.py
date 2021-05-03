059691b25afeec61357facf6fe67cee7cf5d52f0
Henning Schiebenhoefer <henning.schiebenhoefer@posteo.de>
2021-05-03 12:19:16 +0200

    fix: add pyfaidx requirement to requirements-dev.txt

--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
 from distutils.core import setup
 
-setup(name='dudes',
-      description='DUDes: a top-down taxonomic profiler for metagenomics',
-      author='Vitor C. Piro',
-      author_email='vitorpiro@gmail.com',
-      url='https://github.com/pirovc/dudes',
-      version='0.08',
-      packages=['dudes'],
-      scripts=['DUDes.py', 'DUDesDB.py'],
-      install_requires=[
-          "numpy",
-          "pandas",
-          "pyfaidx"
-      ]
-      )
+setup(
+    name="dudes",
+    description="DUDes: a top-down taxonomic profiler for metagenomics",
+    author="Vitor C. Piro",
+    author_email="vitorpiro@gmail.com",
+    url="https://github.com/pirovc/dudes",
+    version="0.08",
+    packages=["dudes"],
+    scripts=["DUDes.py", "DUDesDB.py"],
+    install_requires=["numpy", "pandas", "pyfaidx"],
+)
