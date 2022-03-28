37632d6730f3913a1a736feeeaeae30d166e9c47
Henning Schiebenhoefer <henning.schiebenhoefer@posteo.de>
2022-03-28 20:06:16 +0200

    tests: convert dudes integration test from subprocess to function call

--- test/helper_funcs.py
+++ test/helper_funcs.py
@@ -1,5 +1,6 @@
 import hashlib
 import os
+from contextlib import contextmanager
 from pathlib import Path
 
 RESSOURCE_DIR = Path(os.path.dirname(__file__)) / "ressource"
@@ -10,3 +11,22 @@ SAMPLEDATA_DIR = DUDES_DIR / "sampledata"
 def md5sum(filename):
     data = open(filename, 'rb').read()
     return hashlib.md5(data).hexdigest()
+
+
+@contextmanager
+def set_directory(path: Path):
+    """Sets the cwd within the context
+
+    Args:
+        path (Path): The path to the cwd
+
+    Yields:
+        None
+    """
+
+    origin = Path().absolute()
+    try:
+        os.chdir(path)
+        yield
+    finally:
+        os.chdir(origin)
--- test/test_DUDes.py
+++ test/test_DUDes.py
@@ -1,16 +1,20 @@
-import subprocess
-from test.helper_funcs import RESSOURCE_DIR, DUDES_DIR, SAMPLEDATA_DIR, md5sum
+from DUDes import main
+from test.helper_funcs import RESSOURCE_DIR, DUDES_DIR, SAMPLEDATA_DIR, md5sum, set_directory
+from unittest.mock import patch
+import sys
 
 
 def test_DUDes(tmp_path):
     produced_output = tmp_path / "dudes_profile_output"
     expected_output = RESSOURCE_DIR / "dudes_profile_expected_output_on_sampledata.out"
-    args = [DUDES_DIR / "DUDes.py",
-            "-s", SAMPLEDATA_DIR.relative_to(DUDES_DIR) / "hiseq_accuracy_k60.sam",
-            "-d", SAMPLEDATA_DIR.relative_to(DUDES_DIR) / "arc-bac_refseq-cg_201503.npz",
-            "-o", produced_output
+    args = [str(DUDES_DIR / "DUDes.py"),
+            "-s", str(SAMPLEDATA_DIR.relative_to(DUDES_DIR) / "hiseq_accuracy_k60.sam"),
+            "-d", str(SAMPLEDATA_DIR.relative_to(DUDES_DIR) / "arc-bac_refseq-cg_201503.npz"),
+            "-o", str(produced_output)
             ]
-    subprocess.call(args, cwd=DUDES_DIR)
+    with set_directory(DUDES_DIR):
+        with patch.object(sys, "argv", args):
+            main()
     assert md5sum(produced_output.with_suffix(".out")) == md5sum(expected_output), \
         f'wrong dudes output produced: {produced_output.with_suffix(".out")}\n'\
         f'expected output: {expected_output}'
